0b5b3721b02559ab2c9eef82f6a6794d97ed06d6
HaslanBea <haslannascimento@gmail.com>
2026-01-20 15:56:43 -0300

    about da enquete

--- main.js
+++ main.js
@@ -21,4 +21,5 @@ loadComponent("about-system", "./components/AboutSystem/aboutSystem.html");
 loadComponent("highlights", "./components/Highlights/highlights.html");
 loadComponent("destaque", "./components/destaque/destaque.html");
 loadComponent("footer", "./components/footer/footer.html"); 
+loadComponent("enquete", "./components/enquete/enquete.html");
 
